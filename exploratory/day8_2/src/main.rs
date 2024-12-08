31729935aec16b6beea1e540cd7cb015bb7924e0
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-12-08 14:02:18 +0530

    yes

--- exploratory/day8_2/src/main.rs
+++ exploratory/day8_2/src/main.rs
@@ -13,6 +13,13 @@ fn reverse_midpoint(a: Point, b: Point) -> Point {
         y: 2 * a.y - b.y,
     }
 }
+
+impl Point {
+    fn bounds_check(self, x_max: i64, y_max: i64) -> Option<Self> {
+        (self.x > -1 && self.x < x_max && self.y > -1 && self.y < y_max).then_some(self)
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let map: Vec<Vec<_>> = std::fs::read_to_string("input.txt")?
         .lines()
@@ -42,19 +49,30 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("len(points) = {}", points.len());
     let mut antinotes = BTreeSet::new();
     for (_, points) in points.iter() {
-        for (i, a) in points.iter().enumerate() {
+        for (i, &a) in points.iter().enumerate() {
             if points.len() - 1 == i {
                 break;
             }
-            for b in &points[i + 1..] {
-                let p = reverse_midpoint(*a, *b);
-                if p.x > -1 && p.x < x_max && p.y > -1 && p.y < y_max {
-                    antinotes.insert(p);
+            for &b in &points[i + 1..] {
+                let mut left_on_hold = Some((a, b));
+                let mut right_on_hold = Some((a, b));
+
+                while let Some((a, b)) = left_on_hold.take() {
+                    if let Some(p) = reverse_midpoint(a, b).bounds_check(x_max, y_max) {
+                        left_on_hold.replace((p, a));
+                        antinotes.insert(p);
+                    }
                 }
-                let p = reverse_midpoint(*b, *a);
-                if p.x > -1 && p.x < x_max && p.y > -1 && p.y < y_max {
-                    antinotes.insert(p);
+
+                while let Some((a, b)) = right_on_hold.take() {
+                    if let Some(p) = reverse_midpoint(b, a).bounds_check(x_max, y_max) {
+                        right_on_hold.replace((b, p));
+                        antinotes.insert(p);
+                    }
                 }
+
+                antinotes.insert(a);
+                antinotes.insert(b);
             }
         }
     }
