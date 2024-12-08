6982dd10e7a59dc093c902b9c32a5b23ef704fc0
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-12-08 07:25:32 +0530

    breakneck speed impls

---
+++ breakneck/day7_2/src/main.rs
@@ -0,0 +1,67 @@
+use std::io::stdin;
+
+#[derive(Clone, Debug)]
+enum Op {
+    Add,
+    Mul,
+    Pip,
+}
+
+fn preorder(res: u64, values: &[u64], ops: &[Op]) -> bool {
+    let Some(&top) = values.first() else {
+        return false;
+    };
+    if values.len() == 1 && res == top {
+        return true;
+    }
+    let mut path = false;
+    if res % top == 0 {
+        let mut next_ops = Vec::from(ops);
+        next_ops.push(Op::Mul);
+        path |= preorder(res / top, &values[1..], &next_ops);
+    }
+
+    if res > top {
+        let mut next_ops = Vec::from(ops);
+        next_ops.push(Op::Add);
+        path |= preorder(res - top, &values[1..], &next_ops);
+    }
+
+    // log requires operand to be nonzero
+    if top != 0 && res > top {
+        let power_of_10 = 10u64.pow(top.ilog10() + 1);
+        let delta = res - top;
+        if delta % power_of_10 == 0 {
+            let mut next_ops = Vec::from(ops);
+            next_ops.push(Op::Pip);
+            path |= preorder(delta / power_of_10, &values[1..], &next_ops);
+        }
+    }
+
+    path
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut sum = 0;
+    for line in stdin().lines() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+        let (r, vals) = line.split_once(":").expect("bad line");
+        let r: u64 = r.trim().parse()?;
+        let values = vals
+            .trim()
+            .split(' ')
+            .map(|v| v.trim().parse())
+            .rev()
+            .collect::<Result<Vec<u64>, _>>()?;
+        if !preorder(r, &values, &[]) {
+            continue;
+        };
+
+        sum += r;
+    }
+    println!("calibrated to {sum}");
+    Ok(())
+}
